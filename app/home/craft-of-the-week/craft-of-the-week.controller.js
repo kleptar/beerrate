283c6bc644dbcadafd2cf8579700884fd815f699
Duda, Tomasz <tomasz.duda@dieboldnixdorf.com>
2018-06-06 15:55:57 +0200

    okokok

--- app/home/craft-of-the-week/craft-of-the-week.controller.js
+++ app/home/craft-of-the-week/craft-of-the-week.controller.js
@@ -1,3 +1,7 @@
 angular.module("beer-rate.home").controller("CraftOfTheWeekController", function(){
     var vm = this;
+
+    vm.propsTheBeerForToday = function(){
+        vm.propsBeer(vm.beer);        
+    }
 });
