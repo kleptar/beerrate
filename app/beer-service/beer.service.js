478f3c1442d6d4eac452bcbff37ba3b4315cf75a
Duda, Tomasz <tomasz.duda@dieboldnixdorf.com>
2018-06-06 21:23:41 +0200

    style

--- app/beer-service/beer.service.js
+++ app/beer-service/beer.service.js
@@ -6,13 +6,13 @@ angular.module("beer-rate.home").service("beerService", function(){
 
     function getBeers(){
         return [
-            { name: "Tyskie", brewery: "piwowarnia 1", rate: 2},
-            { name: "Zuber", brewery: "piwowarnia 1", rate: 3},
-            { name: "Volt", brewery: "piwowarnia 1", rate: 5},
-            { name: "Lech", brewery: "rudy browar", rate: 7},
-            { name: "Kasztelan", brewery: "rozklapiocha brew co", rate: 6},
-            { name: "Tatra", brewery: "tys browar", rate: 1},
-            { name: "Harnas", brewery: "tys browar", rate: 8}
+            { id: 1, name: "Tyskie", brewery: "piwowarnia 1", rate: 2},
+            { id: 2, name: "Zuber", brewery: "piwowarnia 1", rate: 3},
+            { id: 3, name: "Volt", brewery: "piwowarnia 1", rate: 5},
+            { id: 4, name: "Lech", brewery: "rudy browar", rate: 7},
+            { id: 5, name: "Kasztelan", brewery: "rozklapiocha brew co", rate: 6},
+            { id: 6, name: "Tatra", brewery: "tys browar", rate: 1},
+            { id: 7, name: "Harnas", brewery: "tys browar", rate: 8}
         ];
     }
 
--- app/home/list-beers/list-beers.controller.js
+++ app/home/list-beers/list-beers.controller.js
@@ -1,3 +1,33 @@
 angular.module("beer-rate.home").controller("ListBeerController", function(){
     var vm = this;
+    vm.beersToShow = [];
+    var activeBeer = -1;
+    vm.initBeerList = function(){
+        vm.beersToShow = vm.beers;
+        angular.forEach(vm.beersToShow,function(value, key) {
+            value.showDetails = false;
+        }); 
+    }
+
+    vm.toggleBeerDetails = function(beerToMark){
+        vm.initBeerList();
+        
+        var selectedBeer = vm.beersToShow.filter(function (beer) {
+            return (beer.id == beerToMark);
+        })[0];
+        deactivateListElements();
+        if (activeBeer==beerToMark) return;
+        document.getElementById("beer-in-row-" + beerToMark).classList.add('active');
+        selectedBeer.showDetails = true;
+        activeBeer = beerToMark;
+    }
+
+    function deactivateListElements(){
+        angular.forEach(vm.beersToShow,function(value, key) {
+            value.showDetails = false;
+            if ( document.getElementById("beer-in-row-" + value.id).classList.contains('active') ) {
+                document.getElementById("beer-in-row-" + value.id).classList.remove('active');
+            }
+        }); 
+    }
 });
