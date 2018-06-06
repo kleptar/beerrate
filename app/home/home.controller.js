7fa2b145da71128537cdfcd5ab44bbc053ffa7e4
Duda, Tomasz <tomasz.duda@dieboldnixdorf.com>
2018-06-06 15:43:16 +0200

    kleptar

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -2,7 +2,7 @@ angular.module("beer-rate.home").controller("HomeController", function(){
     var vm = this;
     vm.zm1 = "zmienna";
     vm.beers = ["Pale ale","IPA","AIPA","APA","Blonde Ale","Witbier","Stout","RIS","Porter"];
-    vm.beerOfTheWeek = beer[2];
+    vm.beerOfTheWeek = vm.beers[2];
     vm.propsTheBeer = function(beer){
         alert("lubisz to suko! " + beer);
     }
